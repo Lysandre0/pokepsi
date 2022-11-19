66f6ff0b14f25080b02769df971d78700461cfa9
Lysandre0 <lysandrelebigot@gmail.com>
2022-11-19 19:12:32 +0100

    connect to API and storage data in database

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=200)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    special_attack = models.IntegerField()
+    special_defense = models.IntegerField()
+    hp = models.IntegerField()
+    speed = models.IntegerField()
+    front_default = models.CharField(max_length=200)
+    back_default = models.CharField(max_length=200)
+    front_shiny = models.CharField(max_length=200)
+    back_shiny = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,5 +1,39 @@
-from django.http import HttpResponse
+import requests
+from django.shortcuts import render
+from .models import Pokemon
 
+#get pokemon data and images from PokeAPI and save to database
+def get_pokemon_data():
+    for i in range(1, 152):
+        url = 'https://pokeapi.co/api/v2/pokemon/' + str(i)
+        response = requests.get(url)
+        pokemon_data = response.json()
+        name = pokemon_data['name'].capitalize()
+        height = pokemon_data['height']
+        weight = pokemon_data['weight']
+        attack = pokemon_data['stats'][4]['base_stat']
+        defense = pokemon_data['stats'][3]['base_stat']
+        special_attack = pokemon_data['stats'][2]['base_stat']
+        special_defense = pokemon_data['stats'][1]['base_stat']
+        hp = pokemon_data['stats'][5]['base_stat']
+        speed = pokemon_data['stats'][0]['base_stat']
+        front_default = pokemon_data['sprites']['front_default']
+        back_default = pokemon_data['sprites']['back_default']
+        front_shiny = pokemon_data['sprites']['front_shiny']
+        back_shiny = pokemon_data['sprites']['back_shiny']
+        pokemon = Pokemon(name=name, height=height, weight=weight, attack=attack, defense=defense, special_attack=special_attack, special_defense=special_defense, hp=hp, speed=speed, front_default=front_default, back_default=back_default, front_shiny=front_shiny, back_shiny=back_shiny)
+        pokemon.save()
 
+#if model is empty, get data from PokeAPI and save to database
+if Pokemon.objects.count() == 0:
+    get_pokemon_data()
+
+#render index page
 def index(request):
-    return HttpResponse("Hello, world. You're at the pokedex index.")
+    pokemon = Pokemon.objects.all()
+    return render(request, 'pokedex/index.html', {'pokemon': pokemon})
+
+#define pokemon_name
+def pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    return render(request, 'pokedex/pokemon.html', {'pokemon': pokemon})
